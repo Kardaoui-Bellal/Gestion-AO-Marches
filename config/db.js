7d3868c2dd015bd6306cfb221915637366be1b36
Kardaoui-Bellal <bellal.kardaoui@edu.uiz.ac.ma>
2026-07-09 11:43:32 +0100

    db.js values testing

--- config/db.js
+++ config/db.js
@@ -1,23 +1,28 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+const required = [
+    "DB_HOST",
+    "DB_PORT",
+    "DB_USER",
+    "DB_PASSWORD",
+    "DB_NAME"
+];
+
+for (const key of required) {
+    if (!process.env[key]) {
+        throw new Error(`Missing environment variable: ${key}`);
+    }
+}
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ormvasm_ao_marches',
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
+  database: process.env.DB_NAME ,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
-pool.getConnection()
-  .then((conn) => {
-    console.log('Connexion MySQL établie avec succès.');
-    conn.release();
-  })
-  .catch((err) => {
-    console.error('Erreur de connexion MySQL :', err.message);
-  });
 
 module.exports = pool;
