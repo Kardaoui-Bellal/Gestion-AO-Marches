0aace41258ef1f88c1f57b25d668630be2f77f87
khaoula-kardaoui <khaoulakardaoui@gmail.com>
2026-07-28 10:24:05 +0200

    delete marche admin+gest commit

--- src/routes/marcheRoutes.js
+++ src/routes/marcheRoutes.js
@@ -7,10 +7,12 @@ router.get("/echeances", requireRole("ADMIN", "GESTIONNAIRE", "CONSULTANT"), mar
 router.get("/", requireRole("ADMIN", "GESTIONNAIRE", "CONSULTANT"), marcheController.list);
 router.get("/new", requireRole("ADMIN", "GESTIONNAIRE"), marcheController.showCreateForm);
 router.get("/:id/edit", requireRole("ADMIN", "GESTIONNAIRE"), marcheController.showEditForm);
+router.get("/:id/fiche", requireRole("ADMIN", "GESTIONNAIRE", "CONSULTANT"), marcheController.ficheView);
 router.get("/:id", requireRole("ADMIN", "GESTIONNAIRE", "CONSULTANT"), marcheController.detail);
 
 router.post("/", requireRole("ADMIN", "GESTIONNAIRE"), marcheController.create);
 router.post("/:id", requireRole("ADMIN", "GESTIONNAIRE"), marcheController.update);
 router.post("/:id/statut", requireRole("ADMIN", "GESTIONNAIRE"), marcheController.updateStatus);
+router.post("/:id/delete", requireRole("ADMIN", "GESTIONNAIRE"), marcheController.remove);
 
 module.exports = router;
